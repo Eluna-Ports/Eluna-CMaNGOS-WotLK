67507bed6cabb842b89e6dbed68dec43bd67976a
Ethan <ebakkedahl@gmail.com>
2021-10-03 02:50:26 -0500

    Mod: Eluna LUA Engine
    -Add Eluna Lua Engine support to core
    -Add Eluna Submodule to repository
    -Add CI check to include Eluna
    -Missing async query support, needs to be researched
    
    Co-Authored-By: Foe <Foereaper@users.noreply.github.com>

--- src/game/Entities/GameObject.h
+++ src/game/Entities/GameObject.h
@@ -861,6 +861,7 @@ class GameObject : public WorldObject
         void Respawn();
         bool IsSpawned() const;
         bool IsSpawnedByDefault() const { return m_spawnedByDefault; }
+        void SetSpawnedByDefault(bool spawn) { m_spawnedByDefault = spawn; }
         uint32 GetRespawnDelay() const override { return m_respawnDelay.count(); }
         void SetRespawnDelay(std::chrono::seconds delay, bool once = false)
         {
