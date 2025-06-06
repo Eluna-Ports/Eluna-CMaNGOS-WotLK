3d787463375e2fb02bbe62c4b408aaa07af4d7b0
Niam5 <ebakkedahl@gmail.com>
2025-06-05 19:36:49 -0700

    Add RegisterEvent support to World State

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -9408,7 +9408,7 @@ void Unit::SetInCombatState(bool PvP, Unit* enemy)
                         enemy->GetCombatManager().TriggerCombatTimer(controller);
                 }
 #ifdef BUILD_ELUNA
-                else if (elunaEvents)
+                else if (elunaMapEvents || elunaWorldEvents)
                 {
                     controller->SetInCombatWith(enemy); // player needs to enter combat with summoned Eluna pet
                     enemy->AddThreat(controller);
